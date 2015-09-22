8a98686b7312b049b78ae0f76fb838050ab50cf0
Milan Stojanovic <milan.stojanovic@appriver.com>
2015-09-22 20:45:55 +0200

    added architecture

---
+++ FoodOrdering.WEB/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FoodOrdering.WEB
+{
+	public class FilterConfig
+	{
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+		}
+	}
+}
---
+++ FoodOrdering.WEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FoodOrdering.WEB.Startup))]
+namespace FoodOrdering.WEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
