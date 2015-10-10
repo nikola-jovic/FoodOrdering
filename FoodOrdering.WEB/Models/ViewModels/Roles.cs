8e8db6fe99fc3c143239a1480271b17cc583d1d2
Milan Stojanovic <stojanovic.milan@outlook.com>
2015-10-10 13:44:20 +0200

    Removing unnecessary role.

--- FoodOrdering.WEB/Models/ViewModels/Roles.cs
+++ FoodOrdering.WEB/Models/ViewModels/Roles.cs
@@ -4,8 +4,6 @@ namespace FoodOrdering.WEB.Models.ViewModels
 {
     public enum Roles
     {
-        [Display(Name = "Regular user")]
-        RegularUser,
         [Display(Name = "Company admin")]
         CompanyAdmin,
         [Display(Name = "Super admin")]
