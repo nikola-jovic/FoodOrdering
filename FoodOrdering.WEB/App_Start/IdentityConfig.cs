50fc655e5d1eaae8cf6c32ab372d03b83b3891b6
Milan Stojanovic <stojanovic.milan@outlook.com>
2015-10-16 14:39:48 +0200

    Now using hosting's database!!!!!! WOOOOOOO!!!!! Also publish settings added and working. Still waiting on the domain transfer.

--- FoodOrdering.WEB/App_Start/IdentityConfig.cs
+++ FoodOrdering.WEB/App_Start/IdentityConfig.cs
@@ -38,7 +38,7 @@ namespace FoodOrdering.WEB
 
         public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context)
         {
-            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
+            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(new ApplicationDbContext()));
             // Configure validation logic for usernames
             manager.UserValidator = new UserValidator<ApplicationUser>(manager)
             {
